868f4c1d63175064d5988f40d0e6a6a59421d628
Muhammad Rifqi Fatchurrahman <muh_rif@live.com>
2024-05-09 19:03:01 +0700

    fix candid autogen

--- src/urunan_backend/src/lib.rs
+++ src/urunan_backend/src/lib.rs
@@ -2,6 +2,7 @@ use crate::core::runtime::Metrics;
 use crate::core::types::{User, UserID, ID};
 use crate::main_module::types::{
     ExpenseDetail, ExpenseQueryFilter, SplitBillDebtor, SplitBillExpense, SplitBillStatus,
+    UserRelIDs,
 };
 use ic_cdk::export_candid;
 
